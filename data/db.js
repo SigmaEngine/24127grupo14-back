14d1c28d58bbbd9956882a2ccf18df55605ae150
SigmaEngine <agustinimanolperezmendivil@gmail.com>
2024-07-15 22:58:48 -0300

    Add files via upload

---
+++ data/db.js
@@ -0,0 +1,9 @@
+const {Sequelize} = require ("sequelize")
+
+
+const db = new Sequelize ("posteos24126","root","",{
+    host: "localhost",
+    dialect:"mysql",
+    port:3306
+})
+module.exports = db
